bb8f3f3c113539cc1bf777fee8efd1345f838b3d
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2024-12-29 22:24:30 +0000

    terraform

---
+++ 50-vpn/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.82.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "expense-infra-remote-state"
+    key    = "expense-dev-vpn"
+    region = "us-east-1"
+    dynamodb_table = "expense-infra-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
