d07c99c23b410594e5e93d9bf29dd072fb698c9f
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2025-01-04 12:33:10 +0000

    terraform

---
+++ 80-web-alb/provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.82.0"
+    }
+  }
+
+  backend "s3" {
+    bucket = "expense-infra-remote-state"
+    key    = "expense-web-alb-dev"
+    region = "us-east-1"
+    dynamodb_table = "expense-infra-locking"
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
