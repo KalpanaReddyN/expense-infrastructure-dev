9361db1aa892cf2e04b390823b363a08841c1e04
KalpanaReddyN <kalpanareddyneelapu@gmail.com>
2025-08-16 11:51:57 +0100

    infra

--- 00-s3bucket_dynamodb_creation/main.tf
+++ 00-s3bucket_dynamodb_creation/main.tf
@@ -7,34 +7,34 @@ resource "aws_s3_bucket" "statefiles_bucket" {
   }
 }
 
-# resource "aws_s3_bucket_policy" "dev_statefile_policy" {
-#   bucket = aws_s3_bucket.statefiles_bucket.id
+resource "aws_s3_bucket_policy" "dev_statefile_policy" {
+  bucket = aws_s3_bucket.statefiles_bucket.id
 
-#   policy = jsonencode({
-#     Version = "2012-10-17"
-#     Statement = [
-#       {
-#         Sid       = "AllowAccountAccess"
-#         Effect    = "Allow"
-#         Principal = {
-#           AWS = "arn:aws:iam::211125410910:root"  # Replace with your AWS Account ID
-#         }
-#         Action = [
-#           "s3:GetObject",
-#           "s3:PutObject",
-#           "s3:DeleteObject",
-#           "s3:ListBucket",
-#           "s3:GetObjectTagging",
-#           "s3:PutObjectTagging"
-#         ]
-#         Resource = [
-#           "arn:aws:s3:::dev_infra_statefile",
-#           "arn:aws:s3:::dev_infra_statefile/*"
-#         ]
-#       }
-#     ]
-#   })
-# }
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "AllowAccountAccess"
+        Effect    = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::211125410910:root"  # Replace with your AWS Account ID
+        }
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject",
+          "s3:DeleteObject",
+          "s3:ListBucket",
+          "s3:GetObjectTagging",
+          "s3:PutObjectTagging"
+        ]
+        Resource = [
+          "arn:aws:s3:::dev_infra_statefile",
+          "arn:aws:s3:::dev_infra_statefile/*"
+        ]
+      }
+    ]
+  })
+}
 
 # resource "aws_s3_bucket_policy" "allow_access_from_another_account" {
 #   bucket = aws_s3_bucket.example.id
